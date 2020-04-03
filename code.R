4363dad4b0778b5192b5015e837a8600b78f3174
ionyango <irenenyagi@gmail.com>
2020-04-02 23:55:15 -0600

    addign a few lines of code

--- code.R
+++ code.R
@@ -2,4 +2,5 @@ install.packages(c("digest", "caTools", "bitops"))
 library(tidyverse)
 library(dslabs)
 library(digest)
-library(bitops)
+library(bitops)Adding a few lines of code
+Adding a few lines of code
