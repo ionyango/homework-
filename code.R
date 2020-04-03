dea2619cd4afc42839cb090bb27052d9925e4eaa
ionyango <irenenyagi@gmail.com>
2020-04-02 23:57:50 -0600

    Added a few lines of code

--- code.R
+++ code.R
@@ -1,6 +1,13 @@
-install.packages(c("digest", "caTools", "bitops"))
+install.packages(tidyverse)
 library(tidyverse)
-library(dslabs)
-library(digest)
-library(bitops)Adding a few lines of code
-Adding a few lines of code
+
+data(murders)
+murders<-mutate(murders, rate==total/population*100000)
+murders<-mutate(murders, population_in_millions=population/10^6)
+head(murders)
+
+murders_rank<- mutate(murders,rank(rate))
+head(murders_rank)
+
+murders_nw<-filter(murders_rank,population<5000000 & region %in% c("Northeast", "West"))
+head(murders_nw)
